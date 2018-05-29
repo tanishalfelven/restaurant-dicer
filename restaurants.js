be7bcb0e82e175d964cdf9e0ab46c52fb44406e1
tanishalfelven <tanishalfelven@users.noreply.github.com>
2018-05-29 16:23:18 -0700

    Split restaurants and fonts to files

---
+++ restaurants.js
@@ -0,0 +1,16 @@
+export const restaurants = [
+    "Kizuki",
+    "Square Lotus/Thai",
+    "Southgate Garden",
+    "Lot No. 3",
+    "Ricardos",
+    "Ooba Tooba",
+    "Taco Time",
+    "Chipotle",
+    "Topgun Seafood",
+    "Dim-Sum Factory",
+    "Mod Pizza",
+    "Jinya Ramen",
+    "Palace BBQ",
+    "Teriyaki & More"
+];
--- script.js
+++ script.js
@@ -1,48 +1,9 @@
-const rest = [
-    "Kizuki",
-    "Square Lotus/Thai",
-    "Southgate Garden",
-    "Lot No. 3",
-    "Ricardos",
-    "Ooba Tooba",
-    "Taco Time",
-    "Chipotle",
-    "Topgun Seafood",
-    "Dim-Sum Factory",
-    "Mod Pizza",
-    "Jinya Ramen",
-    "Palace BBQ",
-    "Teriyaki & More"
-];
-fonts = [
-    "Georgia",
-    "serif",
-    "Palatino Linotype",
-    "Book Antiqua",
-    "Palatino",
-    "Times New Roman",
-    "Times",
-    "Arial",
-    "Helvetica",
-    "sans-serif",
-    "Arial Black",
-    "Gadget",
-    "Comic Sans MS",
-    "cursive",
-    "Impact",
-    "Charcoal",
-    "Lucida Sans Unicode",
-    "Lucida Grande",
-    "Tahoma",
-    "Geneva",
-    "Trebuchet MS",
-    "Helvetica",
-    "Verdana"
-];
+import {restaurants} from "./restaurants.js";
+import {fonts} from "./fonts.js";
 
 function getRand(arr) {
     return arr[Math.floor(Math.random()*arr.length)];
-  }
+}
 
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
@@ -54,15 +15,21 @@ function shuffle(a) {
 
 m.mount(window.mount, {
   view : (vnode) => [
-    m("button", {onclick : () => { 
-      vnode.state.on = !vnode.state.on;
-      if (vnode.state.on) m.redraw(true);
-    }}, vnode.state.on ? "Reset" : "Dice me a restaurant!"),
-    m("div.outer",
-        m("div.inner" + (vnode.state.on ? ".slide" : ".noslide"),
-            shuffle(rest).map((r) =>
-                m("div.item", { style: {fontFamily: getRand(fonts)} }, r)
+        m("div.edit",
+            m("a", { href : "https://github.com/tanishalfelven/restaurant-dicer" }, "Add more options!")
+        ),
+        m("div", 
+            m("button", {onclick : () => { 
+            vnode.state.on = !vnode.state.on;
+            if (vnode.state.on) m.redraw(true);
+            }}, vnode.state.on ? "Reset" : "Dice me a restaurant!"),
+            m("div.outer",
+                m("div.inner" + (vnode.state.on ? ".slide" : ".noslide"),
+                    shuffle(restaurants).map((r) =>
+                        m("div.item", { style: {fontFamily: getRand(fonts)} }, r)
+                    )
+                )
             )
         )
-    )]
+    ]
 })
